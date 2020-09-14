d37a94526a9693f40d55da5e01c09163da3ef8c1
adrian154 <94568a@gmail.com>
2020-09-13 19:58:24 -0700

    we got tiles!!!

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,106 +1,97 @@
-class Wall {
+// Constants
+const Tiles = {
+    WATER: 0,
+    GRASS: 1
+};
 
-    constructor(point1, point2) {
-        this.point1 = point1;
-        this.point2 = point2;
-    }
+const GameStates = {
+    AWATING_SOCKET: 0,
+    AWAITING_GAME_START: 1,
+    GAME_RUNNING: 2,
+    SOCKET_CLOSED: 3
+};
 
-    render(ctx) {
-        ctx.strokeStyle = "#a6a6a6";
-        ctx.lineWidth = 5;
-        ctx.beginPath();
-        ctx.moveTo(this.point1[0], this.point1[1]);
-        ctx.lineTo(this.point2[0], this.point2[1]);
-        ctx.closePath();
-        ctx.stroke();
-    }
+const InputTask = {
+    NONE: 0, // Just scrolling, etc.
+    PLACING_TWO_ENDPOINTS: 1 // Selecting two endpoints
+};
 
-}
+Object.freeze(Tiles);
+Object.freeze(GameStates);
+Object.freeze(InputTask);
+
+// Note to self on rendering:
+// Top level rendering functions call lower level rendering functions
+// Bottom  level rendering functions are responsible for saving and restoring canvas state
 
 class World {
 
     constructor(data) {
-        this.terrain = data;
+        
+        this.data = data;
         this.objects = [];
+
+        // It's assumed that the map is square
+        this.width = data.length;
+        this.height = data[0].length;
+        
+        this.offscreenCanvas = document.getElementById("worldCanvas");
+        this.offscreenCanvas.width = this.width;
+        this.offscreenCanvas.height = this.height;
+
+        this.drawCtx = this.offscreenCanvas.getContext("2d");
+        this.updateCanvas();
+    
     }
 
-    // Drawn in camera space
-    renderTerrain(ctx) {
+    static getColor(tileType) {
+        return ({
+            [Tiles.WATER]: "#00a2e8",
+            [Tiles.GRASS]: "#22b14c"
+        })[tileType];
+    }
 
-        for(let continent of this.terrain) {
+    // Redraw the contents of the offscreen canvas
+    // Don't call this method when an update is received from the server
+    // Certainly don't call it on every frame!
+    // Ideally, this only needs to be called upon initialization
+    updateCanvas() {
 
-            if(continent.length < 1)
-                continue;
-
-            ctx.fillStyle = "#269e3e";
-            ctx.beginPath();
-            ctx.moveTo(continent[0][0], continent[0][1]);
-            for(let i = 1; i < continent.length; i++) {
-                ctx.lineTo(continent[i][0], continent[i][1]);
+        for(let x = 0; x < this.data.length; x++) {
+            for(let y = 0; y < this.data[x].length; y++) {
+                this.drawCtx.fillStyle = World.getColor(this.data[x][y]);
+                this.drawCtx.fillRect(x, y, 1, 1);
             }
-            ctx.closePath();
-            ctx.fill();
-
         }
 
+    }
+
+    // Apply an update from the server
+    applyUpdate(update) {
+        // TODO
+    }
+
+    // Terrain is rendered to an OffscreenCanvas that is blitted to the real canvas.
+    // The offscreen canvas is asynchronously updated when updates come from the server
+    // This avoids iterating over the entire map every frame
+    render(ctx) {
+        this.renderTerrain(ctx);
+        this.renderObjects(ctx);
+    }
+
+    renderTerrain(ctx) {
+        ctx.save();
+        ctx.drawImage(this.offscreenCanvas, -this.offscreenCanvas.width / 2, -this.offscreenCanvas.height / 2);
+        ctx.restore();
     }
 
     renderObjects(ctx) {
-
-        for(let object of this.objects) {
-            let pre = ctx.getTransform();
-            object.render(ctx);
-            ctx.setTransform(pre);
-        }
-
-    }
-
-    
-    getContainingContinent(point) {
-
-        for(let continent of this.terrain) {
-
-            let isects = 0;
-            for(let i = 0; i < continent.length - 1; i++) {
-                
-                let v0 = continent[i];
-                let v1 = continent[i + 1];
-
-                if(inRange(point[1], v0[1], v1[1])) {
-                    let x = interpLinear(v0[1], v1[1], v0[0], v1[0], point[1]);
-                    if(point[0] < x) {
-                        isects++;
-                    }
-                }
-
-            }
-
-            if(isects % 2 == 1) {
-                return continent;
-            }
-
-        }
-
-        return undefined;
-
-    }
-
-    hasClearPath(point1, point2) {
-
-        for(let continent of this.terrain) {
-            for(let i = 0; i < continent.length - 1; i++) {
-                if(intersects(point1, point2, continent[i], continent[i + 1])) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
 
     }
 
 }
 
+// This class is having a bit of an identity crisis
 class Renderer {
 
     constructor(game, canvasElement) {
@@ -109,12 +100,13 @@ class Renderer {
         this.canvas = canvasElement;
         this.ctx = this.canvas.getContext("2d");
 
+        // Initial camera settings
         this.scale = 1;
         this.cameraX = 0;
         this.cameraY = 0;
         this.cameraAngle = 0;
 
-        // For animation, etc.
+        // For animation
         this.frame = 0;
         this.interpScale = false;
 
@@ -124,11 +116,15 @@ class Renderer {
     }
 
     handleResize() {
+
+        // Keep the canvas's native size identical to its actual size
+        // This helps avoid scaling
         let box = this.canvas.getBoundingClientRect();
         this.canvas.width = box.width;
         this.canvas.height = box.height;
-    }
 
+    }
+    
     interpolateScaling() {
         if(this.interpScale) {
             if(this.frame < this.scaleInterpEndFrame) {
@@ -144,6 +140,7 @@ class Renderer {
         this.interpolateScaling();
     }
 
+    // this is the source of much misery over the past month
     doCameraTransform() {
         
         this.ctx.translate(this.canvas.width / 2, this.canvas.height / 2);
@@ -157,26 +154,11 @@ class Renderer {
 
     renderUI() {
 
-        let taskText = "";
-
-        if(this.game.input.currentTask == InputTask.PLACING_TWO_ENDPOINTS) {
-            taskText = `Placing a wall (pick two points by clicking, ${this.game.input.points.length}/2)`;
-        }
-
-        let debugText = `Scale: ${this.scale}`;
-
-        this.ctx.textAlign = "left";
-        this.ctx.font = "24px Arial";
-        this.ctx.fillStyle = "#ffffff";
-        this.ctx.fillText(taskText, 10, 50);
-        this.ctx.fillText(debugText, 10, 70);
-        
-        this.ctx.fillText(`${this.cameraTransform.a}, ${this.cameraTransform.c}, ${this.cameraTransform.e}`, 10, 90);
-        this.ctx.fillText(`${this.cameraTransform.b}, ${this.cameraTransform.d}, ${this.cameraTransform.f}`, 10, 110);
-        this.ctx.fillText(`${0}, ${0}, ${1}`, 10, 130);
+        // Nothing to see here for now
 
     }
 
+    // Remove once project is stable
     drawDebugNub() {
         let wc = this.untransformCoords(this.game.input.mouseX, this.game.input.mouseY);
         this.ctx.fillStyle = "#ff0000";
@@ -184,6 +166,33 @@ class Renderer {
     }
 
     render() {
+
+        if(this.game.state === GameStates.GAME_RUNNING) {
+            this.renderGame();
+        } else {
+            
+            this.ctx.fillStyle = "#000000";
+            this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+
+            this.ctx.fillStyle = "#ffffff";
+            this.ctx.font = "24px Arial";
+            this.ctx.textAlign = "center";
+            
+            let text;
+            switch(this.game.state) {
+                case GameStates.AWAITING_GAME_START: text = "waiting for the game to start..."; break;
+                case GameStates.AWAITING_SOCKET: text = "connecting to the remote server..."; break;
+                case GameStates.SOCKET_CLOSED: text = "remote connection was closed :("; break;
+                default: text = "something extremely bad happened"; break;
+            }
+
+            this.ctx.fillText(text, this.ctx.canvas.width / 2, this.ctx.canvas.height / 2)
+        
+        }
+
+    }
+
+    renderGame() {
 
         // Animate...
         this.updateAnimations();
@@ -205,10 +214,6 @@ class Renderer {
         this.ctx.setTransform(this.cameraTransform);
         this.game.world.renderObjects(this.ctx);
 
-        // draw previews
-        this.ctx.setTransform(this.cameraTransform);
-        this.game.input.renderPreview(this.ctx);
-
         // draw debug nub
         this.ctx.setTransform(this.cameraTransform);
         this.drawDebugNub();
@@ -219,20 +224,7 @@ class Renderer {
 
         // Next frame!
         this.frame++;
-
-    }
-
-    renderNotReady() {
-
-        this.ctx.globalAlpha = 0.5;
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.globalAlpha = 1.0;
-
-        this.ctx.fillStyle = "#ffffff";
-        this.ctx.font = "64px Arial";
-        this.ctx.textAlign = "center";
-        this.ctx.fillText("the game has not started yet", this.canvas.width / 2, this.canvas.height / 2);
-
+        
     }
 
     setScale(x, y, scale) {
@@ -259,14 +251,6 @@ class Renderer {
 
 }
 
-// Tasks
-const InputTask = {
-    NONE: 0, // Just scrolling, etc.
-    PLACING_TWO_ENDPOINTS: 1 // Selecting two endpoints
-};
-
-Object.freeze(InputTask);
-
 class Input {
 
     constructor(game, canvas, renderer) {
@@ -292,7 +276,7 @@ class Input {
         this.canvas.addEventListener("mousemove", event => this.handleMouseMove(event));
 
         // ..on buttons
-        document.getElementById("putRoadButton").addEventListener("click", event => this.handleButtonClick(event, "wall"));
+        //document.getElementById("putRoadButton").addEventListener("click", event => this.handleButtonClick(event, "wall"));
 
         // ..on document
         document.addEventListener("wheel", event => this.handleScroll(event));
@@ -443,13 +427,20 @@ class Game {
 
     constructor() {
 
+        // Connect to server
         this.socket = new WebSocket(`ws://${window.location.host}:8080`);
-        this.ready = false;
+        this.state = GameStates.AWAITING_SOCKET;
 
+        // Set up websocket event listeners
+        this.socket.addEventListener("open", event => { this.state = GameStates.AWAITING_GAME_START });
         this.socket.addEventListener("message", event => this.handleMessage(event));
-        this.socket.addEventListener("close", event => this.ready = false);
+        this.socket.addEventListener("close", event => { this.state = GameStates.SOCKET_CLOSED });
 
+        // Get game canvas
         this.canvas = document.getElementById("gameCanvas");
+        this.ctx = this.canvas.getContext("2d");
+
+        // Set up helper classes
         this.renderer = new Renderer(this, this.canvas);
         this.input = new Input(this, this.canvas, this.renderer);
         this.remote = new Remote(this.socket);
@@ -458,7 +449,7 @@ class Game {
 
     handleGameStart(message) {
     
-        this.ready = true;
+        this.state = GameStates.GAME_RUNNING;
         
         // Set up world
         this.world = new World(message.mapData);
@@ -487,27 +478,10 @@ class Game {
         })[message.type](message);
 
     }
-
-    handleClose(event) {
-
-    }
     
     gameLoop() {
-
-        if(!this.ready) {
-
-            // Render game-not-started message.
-            this.renderer.renderNotReady();
-
-        } else {
-
-            // Render
-            this.renderer.render();        
-        
-        }
-
+        this.renderer.render();        
         requestAnimationFrame(() => this.gameLoop());
-
     }
 
     start() {
